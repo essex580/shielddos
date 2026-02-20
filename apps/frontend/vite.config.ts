c2f01ea4fd65cb3f8643d31ddf9a7d60e6b1d6d0
essex580 <a.sh4dow@yahoo.com>
2026-02-20 20:44:58 +0200

    ci(github-pages): Configure automated Vite build pipeline for Landing Page

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: process.env.NODE_ENV === 'production' ? '/shielddos/' : '/',
     server: {
         host: '0.0.0.0',
         port: 5173,
